2f8d6425a885026c8101b730c6f496765232831f
matsushita <matsushita@fenrir.co.jp>
2017-09-10 21:14:14 +0900

    コメントを追加

--- Assets/Scripts/Tank/TankMovement.cs
+++ Assets/Scripts/Tank/TankMovement.cs
@@ -2,21 +2,21 @@
 
 public class TankMovement : MonoBehaviour
 {
-	public int m_PlayerNumber = 1;
-	public float m_Speed = 12f;
-	public float m_TurnSpeed = 180f;
-	public AudioSource m_MovementAudio;
-	public AudioClip m_EngineIdling;
-	public AudioClip m_EngineDriving;
-	public float m_PitchRange = 0.2f;
+	public int m_PlayerNumber = 1;              // どのタンクがどのプレイヤーに属するかを識別するために使用
+	public float m_Speed = 12f;                 // タンクが前後に移動する速さ
+	public float m_TurnSpeed = 180f;            // タンクが1秒間に何度回転するか
+	public AudioSource m_MovementAudio;         // エンジン音の再生に使用されるオーディオソースへの参照（砲撃オーディオソースとは異なることに注意）
+	public AudioClip m_EngineIdling;            // タンクが動かないときに再生するオーディオ
+	public AudioClip m_EngineDriving;           // タンクが動いているときに再生するオーディオ
+	public float m_PitchRange = 0.2f;           // エンジン音のピッチを変化させる量
 
     
-	private string m_MovementAxisName;
-	private string m_TurnAxisName;
-	private Rigidbody m_Rigidbody;
-	private float m_MovementInputValue;
-	private float m_TurnInputValue;
-	private float m_OriginalPitch;
+	private string m_MovementAxisName;          // 前後の動きの入力軸の名前
+	private string m_TurnAxisName;              // 回転の入力軸の名前
+	private Rigidbody m_Rigidbody;              // タンクを動かすのに使用する参照
+	private float m_MovementInputValue;         // 動きの現在の入力値
+	private float m_TurnInputValue;             // 回転の現在の入力値
+	private float m_OriginalPitch;              // シーン開始時のオーディオソースのピッチ
 
 
 	private void Awake ()
@@ -27,7 +27,10 @@ public class TankMovement : MonoBehaviour
 
 	private void OnEnable ()
 	{
+		// タンクがオンの時、キネマティックでないことを確認
 		m_Rigidbody.isKinematic = false;
+
+		// 入力値もリセット
 		m_MovementInputValue = 0f;
 		m_TurnInputValue = 0f;
 	}
@@ -35,23 +38,25 @@ public class TankMovement : MonoBehaviour
 
 	private void OnDisable ()
 	{
+		// タンクがオフの時、キネマティックに設定
 		m_Rigidbody.isKinematic = true;
 	}
 
 
 	private void Start ()
 	{
+		// 軸の名前はプレイヤー番号に基づく
 		m_MovementAxisName = "Vertical" + m_PlayerNumber;
 		m_TurnAxisName = "Horizontal" + m_PlayerNumber;
 
+		// オーディオソースの元のピッチを格納
 		m_OriginalPitch = m_MovementAudio.pitch;
 	}
 
 
 	private void Update ()
 	{
-		// Store the player's input and make sure the audio for the engine is playing.
-
+		//両方の入力軸の値を保存
 		m_MovementInputValue = Input.GetAxis (m_MovementAxisName);
 		m_TurnInputValue = Input.GetAxis (m_TurnAxisName);
 
@@ -61,25 +66,25 @@ public class TankMovement : MonoBehaviour
 
 	private void EngineAudio ()
 	{
-		// Play the correct audio clip based on whether or not the tank is moving and what audio is currently playing.
-
-		// 移動および回転している間は-1.0f~1.0fなので0の場合は止まっていると判断し、エンジン音を変更する
-		if (Mathf.Abs (m_MovementInputValue) < 0.1f && Mathf.Abs (m_TurnInputValue) < 0.1f) 
+		// タンクが動いているかを判断
+		if (Mathf.Abs (m_MovementInputValue) < 0.1f && Mathf.Abs (m_TurnInputValue) < 0.1f)
 		{
 			if (m_MovementAudio.clip == m_EngineDriving)
 			{
+				// 止まっている音を再生
 				m_MovementAudio.clip = m_EngineIdling;
 				m_MovementAudio.pitch = Random.Range (m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
 				m_MovementAudio.Play ();
 			}
-		} 
+		}
 		else
 		{
 			if (m_MovementAudio.clip == m_EngineIdling)
 			{
+				// 移動している音お再生
 				m_MovementAudio.clip = m_EngineDriving;
-				m_MovementAudio.pitch = Random.Range (m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
-				m_MovementAudio.Play ();
+				m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+				m_MovementAudio.Play();
 			}
 		}
 	}
@@ -87,40 +92,37 @@ public class TankMovement : MonoBehaviour
 
 	private void FixedUpdate ()
 	{
-		// Move and turn the tank.
-
+		// リジッドボディの位置と向きを FixedUpdateで調整します。
 		Move ();
 		Turn ();
 	}
 
+
 	/**
-	 * タンクを移動させる 
+	 * タンクを移動させる
 	 */
 	private void Move ()
 	{
-		// Adjust the position of the tank based on the player's input.
-
-		// 移動するベクトルを計算
+		// 入力、スピード、フレーム間の時間に基づいて、タンクが向いている方向に量のベクトルを作成
 		Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
 
-		// タンクを移動させる
-		m_Rigidbody.MovePosition (m_Rigidbody.position + movement);
+		// この動きをリジッドボディの位置に適用
+		m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
 	}
 
-	/**
-	 *  タンクを回転させる
+
+	/*
+	 * タンクを回転させる
 	 */
 	private void Turn ()
 	{
-		// Adjust the rotation of the tank based on the player's input.
-
-		// 回転する角度を計算
+		// 入力、スピード、フレーム間の時間に基づいて、回転する度数を決定
 		float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
 
-		// Y軸のみ回転するようにする
+		// それを y 軸の回転に設定
 		Quaternion turnRotation = Quaternion.Euler (0f, turn, 0f);
 
-		// タンクを回転させる
+		// この回転をリジッドボディの回転に適用
 		m_Rigidbody.MoveRotation (m_Rigidbody.rotation * turnRotation);
 	}
 }
