5875f64e17718386e42021dad14d43c583766d7e
matsushita <matsushita@fenrir.co.jp>
2017-09-10 21:05:29 +0900

    タンクの作成と制御２

--- Assets/Scripts/Tank/TankMovement.cs
+++ Assets/Scripts/Tank/TankMovement.cs
@@ -2,78 +2,125 @@
 
 public class TankMovement : MonoBehaviour
 {
-    public int m_PlayerNumber = 1;         
-    public float m_Speed = 12f;            
-    public float m_TurnSpeed = 180f;       
-    public AudioSource m_MovementAudio;    
-    public AudioClip m_EngineIdling;       
-    public AudioClip m_EngineDriving;      
-    public float m_PitchRange = 0.2f;
+	public int m_PlayerNumber = 1;
+	public float m_Speed = 12f;
+	public float m_TurnSpeed = 180f;
+	public AudioSource m_MovementAudio;
+	public AudioClip m_EngineIdling;
+	public AudioClip m_EngineDriving;
+	public float m_PitchRange = 0.2f;
 
-    /*
-    private string m_MovementAxisName;     
-    private string m_TurnAxisName;         
-    private Rigidbody m_Rigidbody;         
-    private float m_MovementInputValue;    
-    private float m_TurnInputValue;        
-    private float m_OriginalPitch;         
-
-
-    private void Awake()
-    {
-        m_Rigidbody = GetComponent<Rigidbody>();
-    }
+    
+	private string m_MovementAxisName;
+	private string m_TurnAxisName;
+	private Rigidbody m_Rigidbody;
+	private float m_MovementInputValue;
+	private float m_TurnInputValue;
+	private float m_OriginalPitch;
 
 
-    private void OnEnable ()
-    {
-        m_Rigidbody.isKinematic = false;
-        m_MovementInputValue = 0f;
-        m_TurnInputValue = 0f;
-    }
+	private void Awake ()
+	{
+		m_Rigidbody = GetComponent<Rigidbody> ();
+	}
 
 
-    private void OnDisable ()
-    {
-        m_Rigidbody.isKinematic = true;
-    }
+	private void OnEnable ()
+	{
+		m_Rigidbody.isKinematic = false;
+		m_MovementInputValue = 0f;
+		m_TurnInputValue = 0f;
+	}
 
 
-    private void Start()
-    {
-        m_MovementAxisName = "Vertical" + m_PlayerNumber;
-        m_TurnAxisName = "Horizontal" + m_PlayerNumber;
-
-        m_OriginalPitch = m_MovementAudio.pitch;
-    }
-    */
-
-    private void Update()
-    {
-        // Store the player's input and make sure the audio for the engine is playing.
-    }
+	private void OnDisable ()
+	{
+		m_Rigidbody.isKinematic = true;
+	}
 
 
-    private void EngineAudio()
-    {
-        // Play the correct audio clip based on whether or not the tank is moving and what audio is currently playing.
-    }
+	private void Start ()
+	{
+		m_MovementAxisName = "Vertical" + m_PlayerNumber;
+		m_TurnAxisName = "Horizontal" + m_PlayerNumber;
+
+		m_OriginalPitch = m_MovementAudio.pitch;
+	}
 
 
-    private void FixedUpdate()
-    {
-        // Move and turn the tank.
-    }
+	private void Update ()
+	{
+		// Store the player's input and make sure the audio for the engine is playing.
+
+		m_MovementInputValue = Input.GetAxis (m_MovementAxisName);
+		m_TurnInputValue = Input.GetAxis (m_TurnAxisName);
+
+		EngineAudio ();
+	}
 
 
-    private void Move()
-    {
-        // Adjust the position of the tank based on the player's input.
-    }
+	private void EngineAudio ()
+	{
+		// Play the correct audio clip based on whether or not the tank is moving and what audio is currently playing.
+
+		// 移動および回転している間は-1.0f~1.0fなので0の場合は止まっていると判断し、エンジン音を変更する
+		if (Mathf.Abs (m_MovementInputValue) < 0.1f && Mathf.Abs (m_TurnInputValue) < 0.1f) 
+		{
+			if (m_MovementAudio.clip == m_EngineDriving)
+			{
+				m_MovementAudio.clip = m_EngineIdling;
+				m_MovementAudio.pitch = Random.Range (m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+				m_MovementAudio.Play ();
+			}
+		} 
+		else
+		{
+			if (m_MovementAudio.clip == m_EngineIdling)
+			{
+				m_MovementAudio.clip = m_EngineDriving;
+				m_MovementAudio.pitch = Random.Range (m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+				m_MovementAudio.Play ();
+			}
+		}
+	}
 
 
-    private void Turn()
-    {
-        // Adjust the rotation of the tank based on the player's input.
-    }
+	private void FixedUpdate ()
+	{
+		// Move and turn the tank.
+
+		Move ();
+		Turn ();
+	}
+
+	/**
+	 * タンクを移動させる 
+	 */
+	private void Move ()
+	{
+		// Adjust the position of the tank based on the player's input.
+
+		// 移動するベクトルを計算
+		Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
+
+		// タンクを移動させる
+		m_Rigidbody.MovePosition (m_Rigidbody.position + movement);
+	}
+
+	/**
+	 *  タンクを回転させる
+	 */
+	private void Turn ()
+	{
+		// Adjust the rotation of the tank based on the player's input.
+
+		// 回転する角度を計算
+		float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
+
+		// Y軸のみ回転するようにする
+		Quaternion turnRotation = Quaternion.Euler (0f, turn, 0f);
+
+		// タンクを回転させる
+		m_Rigidbody.MoveRotation (m_Rigidbody.rotation * turnRotation);
+	}
 }
